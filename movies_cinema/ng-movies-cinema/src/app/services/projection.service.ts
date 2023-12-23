f4de77b603583ba0e85f8d9659a567969ea09af6
petar-stojanovic <2001petarstojanovic@gmail.com>
2023-12-23 16:57:10 +0100

    added projections json

--- movies_cinema/ng-movies-cinema/src/app/services/projection.service.ts
+++ movies_cinema/ng-movies-cinema/src/app/services/projection.service.ts
@@ -2,24 +2,24 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Movie} from "../interfaces/movie.interface";
+import {Projection} from "../interfaces/projection.interface";
 
 @Injectable({
   providedIn: 'root'
 })
-export class MovieService {
+export class ProjectionService {
 
-  readonly path = 'api/movie'
+  readonly path = "api/projection"
+
   constructor(private _http: HttpClient) {
   }
 
 
-  getAllMovies(): Observable<Movie[]> {
-    return this._http.get<Movie[]>(`${this.path}`)
-  }
-
-  getMovieById(movieId: number): Observable<Movie> {
+  getAllProjectionsForMovie(movieId: number): Observable<Projection[]> {
     let params = new HttpParams();
     params = params.set("movieId", movieId)
-    return this._http.get<Movie>(`${this.path}`, {params})
+    return this._http.get<Projection[]>(`${this.path}`, {params})
   }
+
+
 }
